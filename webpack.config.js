e1537192b937945947418aed5b3a512d6feb312e
Taito <3835littletaitomonster@gmail.com>
2024-10-17 13:47:28 +0900

    first set

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// webpack.config.js file
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "swc-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader", // Injects styles into DOM
+          "css-loader", // Turns CSS into CommonJS
+          "sass-loader", // Compiles Sass to CSS
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
